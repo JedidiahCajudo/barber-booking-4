89317ded739cd920d39a068a289422db228314d7
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-04-30 17:09:08 +0900

    added devise default views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_secure_password
-  has_many :bookings
-  has_many :barbershops
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  # Example: Check if user is a barber
+  def barber?
+    role == 'barber'
+  end
+
+  # Example: Check if user is a client
+  def client?
+    role == 'client'
+  end
 end
