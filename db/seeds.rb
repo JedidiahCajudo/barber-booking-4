1cf81fd1cff0004c8dfb3b0b2881d6aaca758d65
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-05-05 21:56:25 +0900

    added a faker generator for a barbershop with a barber name but forgot to install the faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Clear old data
+Barbershop.destroy_all
+Barber.destroy_all
+
+# Create 3 barbers
+3.times do
+  barber = Barber.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email,
+    password: 'password'
+  )
+
+  # Each barber owns 1-2 barbershops
+  rand(1..2).times do
+    Barbershop.create!(
+      name: "#{Faker::Hipster.word.capitalize} Cuts",
+      address: Faker::Address.street_address,
+      city: "New York",
+      instagram: "https://instagram.com/#{Faker::Internet.username}",
+      barber: barber
+    )
+  end
+end
