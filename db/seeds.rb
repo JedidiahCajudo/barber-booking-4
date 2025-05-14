7b1e83839a4332d2c90b3ed9917f22a0f0ca0171
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-05-14 14:16:27 +0900

    made a seed again

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,25 @@ require 'faker'
 # Clear old data
 Barbershop.destroy_all
 
+# Create a user (barber)
+user = User.create!(
+  email: 'barber@example.com',
+  password: 'password',
+  role: 'barber'
+)
+
 # Create some barbershops with basic information
 3.times do
-  Barbershop.create!(
+  barbershop = Barbershop.create!(
     name: "#{Faker::Company.name} Barbershop",
     address: Faker::Address.street_address,
     city: "New York",
-    instagram: "https://instagram.com/#{Faker::Internet.username}"
+    instagram: "https://instagram.com/#{Faker::Internet.username}",
+    user: user
   )
+
+  # Add some services to the barbershop
+  barbershop.services.create!(name: "Haircut", price: 20)
+  barbershop.services.create!(name: "Shave", price: 15)
+  barbershop.services.create!(name: "Beard Trim", price: 10)
 end
