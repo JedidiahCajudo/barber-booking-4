208836e0eb79a3b70e69c3e8475e2507fab45753
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-05-14 14:05:42 +0900

    create bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,5 +5,21 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @barbershop = Barbershop.find(params[:barbershop_id])
+    @booking = @barbershop.bookings.new(booking_params)
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to @barbershop, notice: "Booking confirmed!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:service_id, :start_time)
+  end
+
 end
