3c9204f657c2fe71e1fe772ed1f19f8d3b8f0bb9
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-05-14 14:24:13 +0900

    can see the details of barbershop - index, new, show, all fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'bookings/new'
   get 'bookings/create'
+
   devise_for :barbers
   devise_for :users, controllers: {
     sessions: 'users/sessions',
@@ -14,8 +15,10 @@ Rails.application.routes.draw do
   get 'barbers/dashboard', to: 'barbers#dashboard', as: 'barbers_dashboard'
   get 'clients/dashboard', to: 'clients#dashboard', as: 'clients_dashboard'
 
-  # Barbershop routes (make sure these are correct)
-  resources :barbershops, only: [:index, :show]
+  # Barbershop routes (nested bookings resource)
+  resources :barbershops, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
 
   root to: 'home#index'
 end
