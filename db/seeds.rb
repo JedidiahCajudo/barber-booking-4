c29d554dd5ce93d42953a4574793597534968a20
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-05-14 12:00:43 +0900

    just make one barber to use for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,26 @@ UNSPLASH_API_URL = "https://api.unsplash.com/photos/random?query=barbershop&clie
 # Clear old data
 Barbershop.destroy_all
 
-# Create 3 barbershops with random images
-3.times do
-  # Fetch a random barbershop image from Unsplash
-  response = HTTParty.get(UNSPLASH_API_URL)
-  image_url = response.parsed_response['urls']['regular']
+# Create a default barber user
+barber = User.create!(
+  name: Faker::Name.name,
+  email: Faker::Internet.unique.email,
+  password: 'password',
+  role: 'barber'
+)
 
+# Fetch a random barbershop image from Unsplash
+response = HTTParty.get(UNSPLASH_API_URL)
+image_url = response.parsed_response['urls']['regular']
+
+# Create barbershops for the default barber user
+3.times do
   Barbershop.create!(
     name: "#{Faker::Hipster.word.capitalize} Cuts",
     address: Faker::Address.street_address,
     city: "New York",
     instagram: "https://instagram.com/#{Faker::Internet.username}",
+    user: barber,  # Associate with the default barber user
     remote_photo_url: image_url  # Adding a real barbershop image from Unsplash
   )
 end
