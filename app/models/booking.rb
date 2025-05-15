0907330ed7ffc4b6f085d5da910c28d7c7b21f06
Jedidiah Cajudo <jedidiahcajudoace@gmail.com>
2025-05-15 20:28:26 +0900

    tried adding swiper, tried adding unsplash, gave up, started planning for user journey

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,16 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :barbershop
   belongs_to :service
+
+  validates :service_id, presence: true
+  validates :start_time, presence: true
+  validate :check_time_availability
+
+  private
+
+  def check_time_availability
+    if Booking.exists?(barbershop_id: barbershop_id, start_time: start_time)
+      errors.add(:start_time, "is already taken for this barbershop")
+    end
+  end
 end
